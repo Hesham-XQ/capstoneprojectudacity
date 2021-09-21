6817bf47f296a52153ee343ea31de08f16f8d89b
wardahsana <wardahsana@gmail.com>
2021-09-21 10:01:38 -0500

    CF script to create clkuster

---
+++ create-cluster.sh
@@ -0,0 +1,12 @@
+
+
+eksctl create cluster \
+--name capsproj \
+--region us-east-2a \
+--node-type t2.small \
+--nodes 3 \
+--nodes-min 1 \
+--nodes-max 4 \
+--managed
+
+eksctl get cluster --name=capsproj
