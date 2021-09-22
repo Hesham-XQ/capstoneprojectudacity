0023570eba9eb19f63be9e3ceebb0c8a088fb6f8
wardahsana <wardahsana@gmail.com>
2021-09-22 12:47:52 -0500

    corrected create-cluster script

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,12 +1,10 @@
 eksctl create cluster \
---name capsproj \
---region us-east-2a \
+--name capstone \
+--region us-east-2 \
 --node-type t2.small \
---nodes 3 \
---nodes-min 1 \
---nodes-max 4 \
+--nodes 2 \
 --managed
 
-eksctl get cluster --name=capstone
+
 
 
